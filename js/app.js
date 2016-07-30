40b84ff33b77a6940dc51c31092441a509c8b230
steve2482 <stevecb12684@yahoo.com>
2016-07-29 23:07:04 -0400

    guess <li> added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
 
     var randomnumber;
+    var clicks = 1;
 
     /*--- Page Load New Game ---*/
     function newGame(){
@@ -14,15 +15,47 @@ $(document).ready(function(){
     /*--- Click New Game ---*/
     $(".new").click(function() {
         newGame();
+        $("#feedback").text("Make your Guess!");
     });
 
     /*--- Click to Submit Guess ---*/
-
     $("#guessButton").click(function(e) {
         e.preventDefault();
-        if ($("#userGuess").val() > randomnumber + 20) {
+        if ($("#userGuess").val() > randomnumber ) {
+            $("#feedback").text("HOT!!");
+        }
+        else if ($("#userGuess").val() > randomnumber + 5) {
+            $("#feedback").text("Warm!");
+        }
+        else if ($("#userGuess").val() > randomnumber + 10) {
+            $("#feedback").text("Lukewarm!");
+        }
+        else if ($("#userGuess").val() > randomnumber + 20) {
             $("#feedback").text("Cold!");
-        };
+        }
+        else if ($("#userGuess").val() < randomnumber ) {
+            $("#feedback").text("HOT!!");
+        }
+        else if ($("#userGuess").val() < randomnumber - 5) {
+            $("#feedback").text("Warm!");
+        }
+        else if ($("#userGuess").val() < randomnumber - 10) {
+            $("#feedback").text("Lukewarm!");
+        }
+        else if ($("#userGuess").val() < randomnumber - 20) {
+            $("#feedback").text("Cold!");
+        }
+        else  {
+            alert("You are the winner, hahahahaha, you are the winner, hahaahaa");
+        }
+        
+        
+    /*--- # of Guess' Feedback*/
+        $("#count").text(clicks++);
+
+    /*--- #'s Guessed So Far ---*/
+        $("#guessList").append(
+            "<li>" + $("#userGuess").val() + "</li>");      
     });  	
 
     /*--- Display information modal box ---*/
